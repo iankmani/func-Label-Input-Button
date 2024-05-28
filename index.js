2eacc73ff80230d4ce268cdbb9b73d89c3e028cc
Ian Kimani <ian.kiarie2002@gmail.com>
2024-05-28 22:13:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ document.getElementById("myButton").onclick = function(){
   
 }
 //To prompt a inputbox:>>>
-
 // let username = window.prompt("what's Your Name?");
 // console.log(username);
+//check console for answer>
 
-//check console for answer>?
+
+//to change variables to a different datatype
+// let age = window.prompt("how old are you bro?");
+// age = Number(age);
+// age += 1;
+// console.log("happy Birthday bro! you are", age ,"years old.");
+
